eef16954fd7074f7b61eef706089db5941a404da
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-10-17 12:55:51 +0200

    Add files via upload

---
+++ Practica1/FrmProyectos.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Practica1
+{
+    partial class FrmProyectos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titulo = new System.Windows.Forms.Label();
+            this.botonCancelar = new System.Windows.Forms.Button();
+            this.botonOrdenar = new System.Windows.Forms.Button();
+            this.botonImprimir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(154, 67);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(467, 39);
+            this.titulo.TabIndex = 0;
+            this.titulo.Text = "LISTADO DE PROYECTOS";
+            this.titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // botonCancelar
+            // 
+            this.botonCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonCancelar.Location = new System.Drawing.Point(556, 358);
+            this.botonCancelar.Name = "botonCancelar";
+            this.botonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.botonCancelar.TabIndex = 1;
+            this.botonCancelar.Text = "Cancelar";
+            this.botonCancelar.UseVisualStyleBackColor = true;
+            this.botonCancelar.Click += new System.EventHandler(this.botonCancelar_Click);
+            // 
+            // botonOrdenar
+            // 
+            this.botonOrdenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonOrdenar.Location = new System.Drawing.Point(358, 358);
+            this.botonOrdenar.Name = "botonOrdenar";
+            this.botonOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.botonOrdenar.TabIndex = 2;
+            this.botonOrdenar.Text = "Ordenar";
+            this.botonOrdenar.UseVisualStyleBackColor = true;
+            this.botonOrdenar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // botonImprimir
+            // 
+            this.botonImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonImprimir.Location = new System.Drawing.Point(152, 358);
+            this.botonImprimir.Name = "botonImprimir";
+            this.botonImprimir.Size = new System.Drawing.Size(75, 23);
+            this.botonImprimir.TabIndex = 3;
+            this.botonImprimir.Text = "Imprimir";
+            this.botonImprimir.UseVisualStyleBackColor = true;
+            this.botonImprimir.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(152, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(479, 203);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Proyectos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // FrmProyectos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.botonImprimir);
+            this.Controls.Add(this.botonOrdenar);
+            this.Controls.Add(this.botonCancelar);
+            this.Controls.Add(this.titulo);
+            this.Name = "FrmProyectos";
+            this.Text = "FrmProyectos";
+            this.Load += new System.EventHandler(this.FrmProyectos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Button botonCancelar;
+        private System.Windows.Forms.Button botonOrdenar;
+        private System.Windows.Forms.Button botonImprimir;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Practica1/FrmProyectos.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Practica1
+{
+    public partial class FrmProyectos : Form
+    {
+        private ArrayList listaProyectos = new ArrayList();
+        Proyecto p;
+        DateTime fechaIni;
+        DateTime fechaFin;
+
+        public FrmProyectos()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void cargarProyectos()
+        {
+            fechaIni = new DateTime(2021, 2, 3 , 13, 0, 0);
+            fechaFin = DateTime.Today;
+            p = new Proyecto(1, "Best Proyecto", fechaIni, fechaFin, true, 1234.14, 9895.50, false, 1);
+            listaProyectos.Add(p);
+            fechaIni = new DateTime(2022, 5, 21, 8, 0, 0);
+            fechaFin = new DateTime(2023, 9, 30, 9, 0, 0);
+            p = new Proyecto(2, "No muy buen Proyecto", fechaIni, fechaFin, false, 5555.89, 0, true, 2);
+            listaProyectos.Add(p);
+            fechaIni = new DateTime(2020, 7, 14, 6, 0, 0);
+            fechaFin = new DateTime(2020, 10, 3, 10, 0, 0);
+            p = new Proyecto(3, "Fatal", fechaIni, fechaFin, false, 62.0, 0, false, 2);
+            listaProyectos.Add(p);
+            fechaIni = new DateTime(2023, 9, 21, 19, 0, 0);
+            fechaFin = DateTime.Today;
+            p = new Proyecto(2, "Good Proyecto", fechaIni, fechaFin, true, 654.1, 1000.41, false, 3);
+            listaProyectos.Add(p);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void mostrarProyectos()
+        {
+            groupBox1.Controls.Clear();
+            int n = 1;
+            int posicion = 10;
+            foreach (Proyecto project in listaProyectos)
+            {
+                crearEtiqueta(project.Descripcion, posicion, n);
+                posicion += 30;
+                n++;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ordenarProyectos();
+        }
+        
+        private void ordenarProyectos()
+        {
+            groupBox1.Controls.Clear();
+            listaProyectos.Sort();
+            mostrarProyectos();
+        }
+        private void crearEtiqueta(string proyectoText, int posicion, int contadorNombre)
+        {
+            Label LblProyecto = new System.Windows.Forms.Label();
+            LblProyecto.AutoSize = true;
+            LblProyecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F,
+            System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point,
+            ((byte)(0)));
+            LblProyecto.Location = new System.Drawing.Point(75, posicion);
+            LblProyecto.Name = "lblProyecto" + contadorNombre;
+            LblProyecto.Size = new System.Drawing.Size(291, 20);
+            LblProyecto.TabIndex = 1;
+            LblProyecto.Text = proyectoText;
+            groupBox1.Controls.Add(LblProyecto);
+        }
+
+        private void FrmProyectos_Load(object sender, EventArgs e)
+        {
+            cargarProyectos();
+            mostrarProyectos();
+        }
+        private void home_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Are you sure you want to really exit ? ",
+                                    "Exit",
+                                     MessageBoxButtons.YesNo,
+                                     MessageBoxIcon.Question);
+            if (dialog == DialogResult.Yes)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+            else if (dialog == DialogResult.No)
+            {
+                this.Show();
+            }
+        }
+
+        private void botonCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
