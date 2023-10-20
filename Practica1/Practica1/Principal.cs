bedec8dd4a4ee7fca58d73d9168fd373a0981c86
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-10-20 14:10:18 +0200

    20/10/2023

---
+++ Practica1/Practica1/Principal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Principal_FormClosing(object sender,FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        private void modificacionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void facturacionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listadosToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            FrmProyectos Form = new FrmProyectos();
+            Form.ShowDialog();
+        }
+
+        private void altaToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
