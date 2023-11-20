3bdeb329efd84540bb6b8088a3bb93eb751e6db8
Thomas <thomaswfds@gmail.com>
2023-11-20 11:02:10 +0100

    200/11/2023

---
+++ Proyecto/manejadores/ControladorUsuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Manejadores
+{
+    internal static class ControladorUsuarios
+    {
+        public static List<Usuario> listaUsuarios = new List<Usuario>();
+    }
+}
